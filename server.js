15ba09d85d737c058beaaeddd6830ecdb2ea2141
Mat Schaffer <mat@schaffer.me>
2012-10-27 09:35:36 -0400

    A better message

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello world');
+  res.end('Hello from node');
 }).listen(8000);
 
 console.log('Server running at http://*:8000/');
