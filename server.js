07e0f57215c5d52e7a00dfadd75bf86d315bee6d
Mat Schaffer <mat@schaffer.me>
2012-10-27 09:47:11 -0400

    Test message update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from node');
+  res.end('Hello from cposc from node');
 }).listen(8000);
 
 console.log('Server running at http://*:8000/');
